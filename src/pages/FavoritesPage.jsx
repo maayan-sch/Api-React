03d199c02a71ada4ae0f1df9f8ef0626bbe6fcd0
maayanshva <maschwar@g.jct.ac.il>
2026-07-09 01:14:17 +0300

    style: improve UI design and dark mode

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -2,9 +2,11 @@ import PostCard from "../components/PostCard";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function FavoritesPage({ favorites, posts, toggleFavorites }) {
+  const postById = new Map(posts.map((post) => [post.id, post]));
+
   return (
-    <div className="pt-16 bg-[#e7ecef] min-h-screen">
-      <h1 className="text-3xl font-semibold text-[#274c77] pt-5 pb-5">
+    <div className="pt-16 bg-[#e7ecef] dark:bg-[#0d1b2a] min-h-screen">
+      <h1 className="text-3xl font-semibold text-[#274c77] dark:text-[#e0e1dd] pt-5 pb-5">
         My Favorites 🤍
       </h1>
       <motion.div
@@ -15,14 +17,14 @@ export default function FavoritesPage({ favorites, posts, toggleFavorites }) {
         <AnimatePresence>
           {favorites.length === 0 ? (
             <div className="flex min-h-[65vh] items-center justify-center px-6">
-              <div className="flex flex-col items-center text-center gap-4 bg-white/60 backdrop-blur-md shadow-lg rounded-2xl p-10 max-w-md">
+              <div className="flex flex-col items-center text-center gap-4 bg-white/60 dark:bg-[#1b263b] dark:border dark:border-[#415a77] backdrop-blur-md shadow-lg rounded-2xl p-8 max-w-md">
                 <img
                   src="/favorites.svg"
                   alt="No favorites"
-                  className="w-36 md:w-44 opacity-90"
+                  className="w-36 md:w-44 opacity-90 dark:opacity-80"
                 />
 
-                <h2 className="text-2xl font-semibold text-[#8b8c89]">
+                <h2 className="text-2xl font-semibold text-[#8b8c89] dark:text-[#e0e1dd]">
                   no favorites yet
                 </h2>
               </div>
@@ -30,8 +32,8 @@ export default function FavoritesPage({ favorites, posts, toggleFavorites }) {
           ) : (
             <div>
               {favorites.map((id) => {
-                const post = posts.find((post) => post.id === id);
-
+                const post = postById.get(id);
+                if (!post) return null;
                 return (
                   <PostCard
                     key={id}
